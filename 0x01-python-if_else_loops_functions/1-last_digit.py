db0d36fec92bdbfe16d70e34f976b882ace24ee6
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2022-11-30 05:23:28 +0100

    add task 2 on printing letters

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -15,4 +15,4 @@ if lastDigit == 0:
 elif lastDigit > 5 and lastDigit % 10 != 0:
     print(f"{message} and is greater than 5")
 else:
-    print(f"{message} and is less than 6 and not 0")
+    print(f"{message} and is less than 6 and not 0\n")
