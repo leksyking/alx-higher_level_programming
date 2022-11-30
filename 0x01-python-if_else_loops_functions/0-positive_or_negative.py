931be0065bca36895d204a1620da8a6d31763494
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2022-11-30 05:13:59 +0100

    modify the codes

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number} is positive\n")
+    print(f"{number} is positive")
 elif number == 0:
-    print(f"{number} is zero\n")
-elif number < 0:
-    print(f"{number} is less than zero\n")
+    print(f"{number} is zero")
+else:
+    print(f"{number} is less than zero")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str = "Last digit of " + str(number) + " is "
-if number < 0:
-    lastdigit = (-number % 10) * -1
+lastDigit = 0
+
+if number >= 0:
+    lastDigit = number % 10
 else:
-    lastdigit = number % 10
-if lastdigit > 5:
-    print(f"{str} {lastdigit} and is greater than 5")
-elif lastdigit == 0:
-    print(f"{str} {lastdigit} and is zero")
+    lastDigit = (-number % 10) * -1
+
+message = f"Last digit of {number} is {lastDigit}"
+
+if lastDigit == 0:
+    print(f"{message} and is 0")
+elif lastDigit > 5 and lastDigit % 10 != 0:
+    print(f"{message} and is greater than 5")
 else:
-    print(f"{str} {lastdigit} and is less than 6 and not 0")
+    print(f"{message} and is less than 6 and not 0")
