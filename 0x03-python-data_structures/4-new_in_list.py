17267366cbdabc07b10acd0ced47406100da6ee1
root <foogundipe@student.oauife.edu.ng>
2022-12-04 09:41:53 -0800

    add task 6

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,5 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list.copy()
     else:
-        new_string[idx] = element
-        return new_string
+        new_list[idx] = element
+        return new_list
