b4187130cf5778471584a929d30ee5f66ccbd2dc
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2022-12-21 10:22:13 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,17 @@
 
 
 class Square:
+    """ Creating the object instance"""
     def __init__(self. size=0):
+        '''
+            The init method initializes the class instance
+
+        @self:
+            A parameter used to refer to the class instance
+
+        @size:
+            The size of the square, must be a +ve integer
+        '''
         if type(size) is int:
             if size < 0:
                 raise ValueError("Size must be >= 0")
