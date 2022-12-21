39b9b04d79e30ce04a823cf0888ebcf4e3bb5fd2
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2022-12-21 10:37:09 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,10 +14,8 @@ class Square;
         @size:
             The size of the square, must be a +ve integer
         """ 
-        if type(size) is int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        else:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+         self.__size = size
