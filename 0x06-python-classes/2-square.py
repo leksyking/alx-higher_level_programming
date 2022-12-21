83108e8ef26b5d6d9e722a95216f7e4e7dfa6de2
root <foogundipe@student.oauife.edu.ng>
2022-12-21 01:39:41 -0800

    fix a bug in task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ A Module that creates a Square object """ 
 
-class Square;
+class Square:
 """  Creating an Object template """ 
 
     def __init__(self, size=0):
@@ -18,4 +18,4 @@ class Square;
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-         self.__size = size
+        self.__size = size
