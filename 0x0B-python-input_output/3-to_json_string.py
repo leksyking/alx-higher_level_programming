6a52ce025bc0dced2772761e9a998c326d3365fe
root <foogundipe@student.oauife.edu.ng>
2023-01-10 16:53:35 -0800

    add task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function that returns the JSON representation of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of the object"""
+    return json.dumps(my_obj)
