68ef4baaadbafd5569d0b63b17e59fd76392c030
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2023-03-14 12:19:54 +0100

    use another format of for loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const i = 0; i < list.length; i++) {
-  console.log(list[i]);
+const lists = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const list of lists) {
+  console.log(list);
 }
