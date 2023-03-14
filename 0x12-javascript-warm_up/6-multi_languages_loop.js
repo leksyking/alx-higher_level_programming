c624e433743d312676eb697f314acf15caaffc6d
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2023-03-14 12:14:30 +0100

    update let

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < list.length; i++) {
+for (const i = 0; i < list.length; i++) {
   console.log(list[i]);
 }
