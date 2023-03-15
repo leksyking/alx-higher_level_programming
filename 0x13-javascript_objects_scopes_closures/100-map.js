324fd8d5782ecf41657f4ac33ac4e085a70eb274
root <foogundipe@student.oauife.edu.ng>
2023-03-15 16:45:08 -0700

    make the file executable

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const list = require("./100-data").list;
+const list = require('./100-data').list;
 console.log(list);
-console.log(list.map((item, index) =>  index * item));
+console.log(list.map((item, index) => index * item));
