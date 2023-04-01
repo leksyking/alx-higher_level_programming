fa9b677f31bfbf82a5fd7ff582b31440ec387499
leksyking <foogundipe@student.oauife.edu.ng>
2023-03-31 22:07:19 -0700

    use with statement in task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,10 +10,10 @@ def main():
     Function that fetches https://intranet.hbtn.io/status
     """
     url = 'https://intranet.hbtn.io/status'
-    r = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    with requests.get(url) as r:
+        print('Body response:')
+        print('\t- type: {}'.format(type(r.text)))
+        print('\t- content: {}'.format(r.text))
 
 
 if __name__ == "__main__":
